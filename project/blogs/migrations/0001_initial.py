74f4050b65a187c0052c9a1062b2c6201416ae2f
Maria R <rivanenkova@gmail.com>
2017-12-17 20:52:39 +0300

    Fix dependency in migration

--- project/blogs/migrations/0001_initial.py
+++ project/blogs/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('users', '0001_initial'),
     ]
 
     operations = [
